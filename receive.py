2fad4895f76d84a526b11aed6a0cd55e4d3619be
chissie13 <0923272@hr.nl>
2021-01-15 11:35:49 +0100

    updated receive.py

--- receive.py
+++ receive.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import array
 
 UDP_IP = "10.0.0.1" 	#IP to listen to
 UDP_PORT = 4242		# port to listen to
@@ -10,35 +11,75 @@ sock.bind((UDP_IP, UDP_PORT))				#bind the socket to the IP and port
 print("start")
 cumData = ""
 lrpn = -1 #lastReceivedPacketNumber
+busy = False
+list = []
+megaList = []
+packetCounter = 0
+inTransmission = False
 
-def reset():
-	print('reset has been called')
-	lrpn = -1
-	cumData = ""
+def checkChecksum(string, givenChecksum):
+	string = string.encode()
+	bytes = array.array('b', string)
+	checksum = 0
+	for i in range(len(bytes)):
+		checksum ^= bytes[i]
+	checksum = hex(checksum)
+	print('checksum receiver: ' + checksum + '\n')
+	if(checksum[2:] == givenChecksum):
+		return True
+	return False
+
+def verifyFile(list, packetCounter):
+	for x in range(packetCounter):
+		if(list[x] == ''):
+			print('file not complete, some packets went missing\n')
+			return False
+	return True
 
 while True:
 	data, addr = sock.recvfrom(4096)	#receive data
 	data = data.decode('utf-8')
 	packetNumber = int(data[0])
-	if(packetNumber - 1 != lrpn):
-		reset()
-		print('Packet lost, reseting...')
-		if(packetNumber != 0):
-			continue
+	packetCounter += 1
 	data = data[1:]
-	cumData += data				#gather the data and make one long string out of it
-	print("Received data: " + data + '\n')
-	lrpn += 1
-	if(lrpn > 9):
-		lrpn = 0
-	if(cumData.count('|') >= 2):		#if the string contains 2 or more special tokens
-		cumData = cumData[:-1]		#get the filename and data and write it to the file
-		fileData = cumData.split('|')
-		filename = fileData[0]
-		file = fileData[1]
-		print("saved new file:")
-		print(filename + '\n')
-		f = open(filename, 'w')
-		f.write(file)
-		f.close()
-		reset()
+	print('new Packet: ' + data + '\n')
+	if(packetNumber == 0):
+		inTransmission = True
+		lrpn = -1
+		if(list != []):
+			megaList.append(list)
+			list = []
+		packetCounter = 0
+		for x in megaList:
+			if(data in x[0]):
+				list = x
+				megaList.remove(list)
+	if(not inTransmission):
+		continue
+	if(packetNumber < lrpn):
+		packetNumber += 9
+	for x in range(packetNumber - (lrpn + 1)):
+		list.append('')
+		packetCounter += 1
+	if(len(list) > packetCounter):
+		list[packetCounter] = data
+	else:
+		list.append(data)
+	if(packetNumber != 0 and '|' in data):
+		if(verifyFile(list, packetCounter)):
+			string = ''.join(list)
+			fileData = string.split('|')
+			string = fileData[0] + '|' + fileData[1]
+			fileWithoutChecksum = string[:-2]
+			checksum = string[-2:]
+			if(checkChecksum(fileWithoutChecksum, checksum)):
+				fileData = fileWithoutChecksum.split('|')
+				f = open(fileData[0], 'w')
+				f.write(fileData[1])
+				f.close()
+				print('file has been written\n')
+				list = []
+				inTransmission = False
+			else:
+				print('checksum was not equal')
+	lrpn = packetNumber
